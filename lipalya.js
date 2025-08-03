d9e7ccff3d02a6d1876f80b1d79c60a4f6e5b4a1
LippWangsaff <syylip@gmail.com>
2025-08-03 18:50:43 +0700

    Update lipalya.js

--- lipalya.js
+++ lipalya.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const settingsMenuDropdown = document.getElementById('settings-menu-dropdown');
     const currentModeText = document.getElementById('current-mode-text');
 
-    const API_KEY = 'AIzaSyAhXd4KXP3bHa2cJz44oLyYGVYXUU8Lqug';
+    const API_KEY = 'AIzaSyBKKZMbYnAEfe5jG7PhmLZzV9ankV93Lyc';
     const MODEL_NAME = 'gemini-2.0-flash';
     const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/${MODEL_NAME}:generateContent?key=${API_KEY}`;
     
